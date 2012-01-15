418aa0f6890f5250cd2078be54713c782e60f515
Nicolas Petton <petton.nicolas@gmail.com>
2012-01-15 16:46:14 +0100

    removed a comment block

--- hooks/init-org.el
+++ hooks/init-org.el
@@ -34,33 +34,3 @@
       (org-export-as-html 3 nil nil nil t "~/projects/nicolas-petton.fr/jekyll/_posts/")))
 
 (add-hook 'after-save-hook 'automatic-org-blog-export-as-html)
-
-
-;; (setq org-publish-project-alist
-;;       '(
-	
-;; 	("org-nicolas-petton.fr"
-;; 	 ;; Path to org files.
-;; 	 :base-directory "~/projects/nicolas-petton.fr/org/"
-;; 	 :base-extension "org"
-	 
-;; 	 ;; Path to Jekyll project.
-;; 	 :publishing-directory "~/projects/nicolas-petton.fr/jekyll/"
-;; 	 :recursive t
-;; 	 :publishing-function org-publish-org-to-html
-;; 	 :headline-levels 4 
-;; 	 :html-extension "html"
-;; 	 :body-only t ;; Only export section between <body> </body>
-;; 	 )
-	
-	
-;; 	("org-static-nicolas-petton.fr"
-;; 	 :base-directory "~/projects/nicolas-petton.fr/org/"
-;; 	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf"
-;; 	 :publishing-directory "~/projects/nicolas-petton.fr/"
-;;           :recursive t
-;;           :publishing-function org-publish-attachment)
-	
-;; 	("nicolas-petton.fr" :components ("org-nicolas-petton.fr" "org-static-nicolas-petton.fr"))
-	
-;; 	))
