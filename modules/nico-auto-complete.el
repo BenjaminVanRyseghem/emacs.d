563ec3bf8f68e7c7a5d8b731c15a83e0a49df475
Nicolas Petton <petton.nicolas@gmail.com>
2014-02-19 23:09:55 +0100

    fixes auto-complete settings

--- modules/nico-auto-complete.el
+++ modules/nico-auto-complete.el
@@ -2,21 +2,21 @@
 (require 'auto-complete)
 (require 'auto-complete-config)
 
-(add-to-list 'ac-user-dictionary 
-			 user-mail-address
-			 "Nicolas"
-			 "Petton")
+(dolist (entry `(,user-mail-address "Nicolas" "Petton"))
+  (add-to-list 'ac-user-dictionary entry))
 
-(setq ac-use-menu-map t)
-
-(add-to-list 'ac-sources 'ac-source-yasnippet)
-(add-to-list 'ac-sources 'ac-source-filename)
-(add-to-list 'ac-sources 'ac-user-dictionary)
+(setq ac-use-menu-map t
+	  ac-ignore-case t)
 
 (setq ac-source-yasnippet nil)
 (ac-flyspell-workaround)
 (ac-config-default)
 
 (global-auto-complete-mode)
+
+(add-hook 'auto-complete-mode-hook 
+		  (lambda ()
+			(dolist (source '(ac-source-yasnippet ac-source-filename ac-source-dictionary))
+			  (add-to-list 'ac-sources source))))
 
 (provide 'nico-auto-complete)
--- modules/nico-js.el
+++ modules/nico-js.el
@@ -6,7 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
  
-(add-hook 'js2-mode-hook '(lambda ()
+(add-hook 'js2-mode-hook (lambda ()
 							;; (flycheck-mode t)
 							(ac-js2-mode)
 							(setq tab-width 4
@@ -18,7 +18,7 @@
 ;; js-comint -- M-x run-js
 (setq inferior-js-program-command "node")
 (setenv "NODE_NO_READLINE" "1")
-(add-hook 'js2-mode-hook '(lambda () 
+(add-hook 'js2-mode-hook (lambda () 
 			    (local-set-key "\C-x\C-e" 'js-send-last-sexp)
 			    (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
 			    (local-set-key "\C-cb" 'js-send-buffer)
